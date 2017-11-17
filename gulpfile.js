72acf47ec120220123a42dd7be66f8f6578c86ca
EON\eon <ekse0n@mail.ru>
2017-11-17 10:54:04 +0300

    Добавил слежку за фаилами

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('less', function() {
   gulp.src('css/*less')
       .pipe(less())
+      .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+      }))
       .pipe(gulp.dest('css'))
 });
 
-gulp.task('autoprefixer', function() {
-	gulp.src('css/style.css')
-		.pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-    }))
-        .pipe(gulp.dest('css'));
-});
+gulp.task('less:watch', function() {
+    gulp.watch('css/*less',['less']);
+    gulp.watch('css/blocks/*less',['less']);
+});    
